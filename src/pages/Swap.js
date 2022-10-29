1983cbd0169abffcda74f363ab40a4382ace2848
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-10-29 11:54:34 +0530

    ui change

--- src/pages/Swap.js
+++ src/pages/Swap.js
@@ -1,28 +1,31 @@
 import React from 'react'
-
+import { Select } from '@chakra-ui/react';
+import {MdArrowDropDown} from 'react-icons/md'
 const Swap = () => {
   return (
     <div className='flex flex-col justify-center items-center'>
-        <div className='flex flex-col justify-center w-[32%] border-2 mt-[60px] rounded-2xl bg-opacity-10 bg-white'>
+        <div className='flex flex-col w-[32%] border-2 mt-[60px] rounded-2xl bg-opacity-10 bg-white'>
             <label className='text-left pl-[15px] text-white my-[10px] font-semibold'>Swap</label>
 
-            <div className=''>
-                <input className='w-[95%] h-[80px] rounded-xl text-4xl pl-[10px]' placeholder='0'/>
-                <div className=''>
-
-                </div>
+            <div className='flex flex-row justify-between bg-white mx-[10px] py-[20px] rounded-xl mb-[5px]'>
+                <input className='w-[240px] ml-[10px] h-[50px] text-4xl' placeholder='0'/>
+                <Select icon={<MdArrowDropDown />} placeholder='Select token' className='my-[10px] text-xl text-center font-semibold'>
+                    <option value='MATIC'>MATIC</option>
+                    <option value='USDC'>USDC</option>
+                </Select>
             </div>
 
-            <br/>
 
-            <div className=''>
-            <input className='w-[95%] h-[80px] rounded-xl text-4xl pl-[10px]' placeholder='0'/>
+            <div className='flex flex-row justify-between bg-white mx-[10px] py-[20px] rounded-xl mb-[20px]'>
+                <input className='w-[240px] ml-[10px] h-[50px] text-4xl' placeholder='0'/>
+                <Select placeholder='Select token' className='my-[10px] text-xl text-center font-semibold'>
+                    <option value='MATIC'>MATIC</option>
+                    <option value='USDC'>USDC</option>
+                </Select>
             </div>
-
-            <br/>
             
             <div>
-            <button className='w-[95%] h-[60px] rounded-xl bg-slate-200 text-black mb-[10px] font-semibold text-xl'>Select a token</button>
+            <button className='w-[95.5%] h-[60px] rounded-xl bg-slate-200 text-black mb-[10px] font-semibold text-xl'>Select a token</button>
             </div>
         </div>
     </div>
